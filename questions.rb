7f6a9c3357c7f12675204fa6dfec9a6ee8de8e6a
Sunny Hundal <amandip.h@gmail.com>
2023-10-24 11:34:41 -0700

    Set logic for Player class

---
+++ questions.rb
@@ -0,0 +1,22 @@
+class Questions
+
+def initialize ()
+  @num1 = rand(1..20)
+  @num2 = rand(1..20)
+end
+
+def get_num1
+  @num1
+end
+
+def get_num2
+  @num2
+end
+
+def ask_question
+  puts "What does #{@num1} plus #{@num2} equal?"
+end
+
+def answer
+@answer =  @num1 + @num2
+end
